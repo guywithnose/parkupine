f28030de8404e7975a611e38fcb9b39948258956
Robert Bittle <guywithnose@gmail.com>
2014-10-16 12:27:51 -0400

    Add yeoman build

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function (grunt) {
             options: {
                 port: 9000,
                 // change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
+                hostname: '*'
             },
             livereload: {
                 options: {
