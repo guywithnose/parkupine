a447cb6c4669fb65603dbd532d4302b9f3c32b84
Robert Bittle <guywithnose@gmail.com>
2014-10-16 13:27:28 -0400

    Update the info windows

--- app/js/map.js
+++ app/js/map.js
@@ -1,3 +1,4 @@
+var infoWindows = [];
 function initialize() {
   var mapOptions = {
     center: { lat: 36.8575964, lng: -76.2795736},
@@ -10,21 +11,38 @@ function initialize() {
     for (var index in garages) {
       var garage = garages[index];
       garage.map = map;
-      garage.icon='/images/map-point.png';
+      garage.availableSpaces = Math.floor(garage.totalSpaces * (.10 + .9 * Math.random()));
       var content = $('<ul>');
       content.append($('<li>').append('Title: ' + garage.title));
       content.append($('<li>').append('Address: ' + garage.address));
-      content.append($('<li>').append('Available spaces: ' + Math.floor(garage.totalSpaces * .34)));
-      content.append($('<li>').append('Price/hr: ' + new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(garage.pricePerHr)));
+      content.append($('<li>').append('Total spaces: ' + Math.floor(garage.totalSpaces * .34)));
+      content.append($('<li>').append('Available spaces: ' + garage.availableSpaces));
+      var percentFull = Math.floor(garage.availableSpaces * 100 / garage.totalSpaces);
+      content.append($('<li>').append('% full: ' + percentFull + '%'));
+      content.append($('<li>').append('Price/hr: ' + garage.pricePerHr.toFixed(2)));
+      if (percentFull > 90) {
+        garage.icon='/images/map-point.png';
+      } else if (percentFull > 50) {
+        garage.icon='/images/map-point.png';
+      } else {
+        garage.icon='/images/map-point.png';
+      }
       var infowindow = new google.maps.InfoWindow({
         content: $('<div>').append(content).html()
       });
       var marker = new google.maps.Marker(garage);
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map, marker);
-      });
+      var infowindow = new google.maps.InfoWindow({content: $('<div>').append(content).html()});
+      infoWindows.push(infowindow);
+      google.maps.event.addListener(marker, 'click', _.partial(showInfoWindow, map, marker, infowindow));
     }
   });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+function showInfoWindow(map, marker, infowindow) {
+  $.each(infoWindows, function(index, value) {
+    this.close();
+  });
+  infowindow.open(map, marker);
+}
